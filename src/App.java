55247dea9a417a07684edd85d1856b1a1f5a9263
rrrroo <r.rochebloine@gmail.com>
2023-12-21 12:32:51 +0100

    Début de menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
+
+    public static Automaton menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Create new automaton");
+        System.out.println("2. Choose between preexisting automaton");
+        System.out.println("3. Load automaton from file");
+        int choice = 0;
+        while (choice != 1 && choice != 2 && choice != 3) {
+            System.out.print("Choice: ");
+            choice = scanner.nextInt();
+        }
+        switch (choice) {
+            case 1:
+                scanner.close();
+                return createAutomaton();
+            case 2:
+                scanner.close();
+                return chooseAutomaton();
+            case 3:
+                System.out.print("Filename: ");
+                String filename = scanner.next();
+                scanner.close();
+                return new Automaton(filename);
+            default:
+                scanner.close();
+                System.out.println("Invalid choice");
+                return null;
+        }
+    }
+
+    public static Automaton createAutomaton() {
+        return null;
+    }
+
+    public static Automaton chooseAutomaton() {
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Automaton auto = menu();
+        System.out.println(auto);
+
     }
 }
