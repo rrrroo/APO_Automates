73a98f79cc4247758691af6bdbb33c4d55b19602
Dorian <dorian.tonnis@hotmail.fr>
2024-01-17 23:04:47 +0100

    évaluation 1D fonctionelle !!!

--- src/app/Test.java
+++ src/app/Test.java
@@ -1,11 +1,12 @@
 package app;
 
 import automaton.*;
+import automaton.grid.*;
 
 public class Test {
 	public static void main(String[] args) {
 		try {
-			Simulation simulation = new Simulation(new Automaton("data/test.json"));
+			Simulation simulation = new Simulation(new Automaton("data/1D.json"));
 			simulation.getAutomaton().getGrid().setAllRandom(simulation.getAutomaton().getAlphabet());
 			simulation.run();
 		} catch (Exception e) {
