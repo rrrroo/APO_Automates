73dccfc80f954cd641c58e48622c527a6db74a4e
Dorian <dorian.tonnis@hotmail.fr>
2024-01-18 12:03:33 +0100

    Update automaton dimensions and apply rules in 3D

--- src/app/Test.java
+++ src/app/Test.java
@@ -6,7 +6,7 @@ import automaton.grid.*;
 public class Test {
 	public static void main(String[] args) {
 		try {
-			Simulation simulation = new Simulation(new Automaton("data/majority2D.json"));
+			Simulation simulation = new Simulation(new Automaton("data/majority3D.json"));
 			simulation.getAutomaton().getGrid().setAllRandom(simulation.getAutomaton().getAlphabet());
 			// simulation.getAutomaton().getGrid().setCellState(0, 1, '1');
 			simulation.run();
