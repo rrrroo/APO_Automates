dc08757a9f4230a5602d263c82d60a08d2260102
rrrroo <r.rochebloine@gmail.com>
2024-02-03 23:28:02 +0100

    Javadoc class window

--- src/main/ui/Window.java
+++ src/main/ui/Window.java
@@ -4,14 +4,41 @@ import javax.swing.*;
 
 import main.automaton.Automaton;
 
+/**
+ * This class is a superclass for the different windows of the application.
+ * It contains the common attributes and methods of the windows.
+ */
 public class Window {
+    /**
+     * The automaton to be displayed in the window.
+     */
     protected Automaton automaton;
+
+    /**
+     * The frame of the window.
+     */
     protected JFrame frame;
+
+    /**
+     * The panel where the automaton is drawn.
+     */
     protected JPanel drawPanel;
+
+    /**
+     * The panel where the controls of the automaton are displayed.
+     */
     protected JPanel controlPanel;
 
+    /**
+     * The draw size of the cells in pixels.
+     */
     protected int cellSize;
 
+    /**
+     * Constructor for the Window class.
+     * @param automaton The automaton to be displayed in the window.
+     * @param cellSize The size of the cells in the automaton.
+     */
     public Window(Automaton automaton, int cellSize){
         this.automaton = automaton;
         this.frame = new JFrame();
@@ -20,11 +47,19 @@ public class Window {
         this.cellSize = cellSize;
     }
 
+    /**
+     * Method to display the window.
+     */
     public void display(){
         this.drawPanel.repaint();
         this.frame.setVisible(true);
     }
 
+    /**
+     * Function to get the next state in the alphabet of the automaton.
+     * @param state The current state.
+     * @return The next state in the alphabet.
+     */
     public char getNextState(char state){
         if(state == automaton.getAlphabet()[automaton.getAlphabet().length-1]){
             return automaton.getAlphabet()[0];
